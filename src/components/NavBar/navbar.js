fb88f19cc3b4dfe075976273dbf6be02f7e420e4
Code1705 <116907909+Code1705@users.noreply.github.com>
2024-09-05 12:41:36 +0530

    chore: Add navbar component and update styling

---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./navbar.css";
+import logo from "../../assets/name.jpg";
+import { Link } from "react-scroll";
+
+const navbar = () => {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="logo" className="navbarLogo" />
+      <div className="desktopMenu">
+        <Link className="desktopMenuListItem">Home</Link>
+        <Link className="desktopMenuListItem">About</Link>
+        <Link className="desktopMenuListItem">Portfolio</Link>
+      
+      </div>
+
+      <button className="desktopMenuButton">
+        <img src="" alt="" className="=desktopMenuImg" />
+        Contact Me
+      </button>
+    </nav>
+  );
+};
+
+export default navbar;
